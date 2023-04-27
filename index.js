485236165f33f22132a782d76734d1072e012bc5
beeak <dudetitan@gmail.com>
2023-04-27 15:53:19 +0300

    Some extras

--- index.js
+++ index.js
@@ -29,3 +29,21 @@ function reveal2() {
   }
 
 window.addEventListener("scroll", reveal2);
+
+
+var string = "Getting any new project ideas, Thinking I could help with anything Or just want to get in contact with me";
+var str = string.split("");
+var tt = document.getElementById('typedtext');
+
+function delay(func, delayTime) {
+    setTimeout(func, delayTime);
+};
+
+function typing() {
+
+    str.length > 0 ? tt.innerHTML += str.shift() : clearTimeout(running); 
+    var running = setTimeout(typing, 30);
+
+};
+
+delay(function() {typing()}, 1000);
